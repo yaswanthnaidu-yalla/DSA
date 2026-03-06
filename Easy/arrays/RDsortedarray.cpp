e58326711d48f7239aa9a46b9b9e30e7fbce9458
unknown <yaswanthnaiduyalla@gmail.com>
2026-03-06 11:55:52 +0530

    removing duplicates from a sorted array

---
+++ Easy/arrays/RDsortedarray.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.empty()) return 0;
+        int k =1;
+        for(int i=1;i<nums.size();i++){
+            if (nums[i]!=nums[i-1]){
+                nums[k]=nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+};
