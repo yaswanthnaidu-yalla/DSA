19bbd5297df729f456ebecfc80e3a4b0e3bc2d33
unknown <yaswanthnaiduyalla@gmail.com>
2026-03-06 11:59:06 +0530

    search insert position in a sorted array

---
+++ Easy/arrays/search_insert_position.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+      if(nums.empty()) return 0;
+      
+      for(int i=0;i<nums.size();i++){
+        if(nums[i]==target){
+            return i;
+        }
+        else if(nums[i]>target){
+             return i;
+        }
+               
+      }
+    
+      
+       return nums.size();
+    }
+};
